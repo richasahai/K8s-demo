75bdd908f40f9405d3cc30e7d604130d5ecd1dc9
vikul <you@example.com>
2024-07-15 20:41:54 +0530

    first commit

---
+++ src/main/java/dev/first/mentor_demo/MentorDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.first.mentor_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/first/mentor_demo/MentorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.first.mentor_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
